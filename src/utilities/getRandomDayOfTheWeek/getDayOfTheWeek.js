ebab24651c94e2b547a3f2424340a46c31c9a2da
Marcin <wanago.marcin@gmail.com>
2023-12-01 16:44:30 +0100

    feat(getRandomDayOfTheWeek): add a function that returns a random day of the week

---
+++ src/utilities/getRandomDayOfTheWeek/getDayOfTheWeek.js
@@ -0,0 +1,12 @@
+export function getDayOfTheWeek(dayNumber) {
+  const weekdays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+  return weekdays[dayNumber];
+}
