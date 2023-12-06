f90569a80f60ab2e73563bf48c298c4025106498
Marcin <wanago.marcin@gmail.com>
2023-12-06 15:47:18 +0100

    Test what happends when the getPosts function responds with an error

--- src/utilities/getPostsSortedByTitle/getPostsSortedByTitle.test.js
+++ src/utilities/getPostsSortedByTitle/getPostsSortedByTitle.test.js
@@ -1,9 +1,16 @@
 import { getPostsSortedByTitle } from './getPostsSortedByTitle';
+import { getPosts } from './getPosts';
 
 jest.mock('./getPosts', () => {
   return {
-    getPosts: () => {
-      return Promise.resolve([
+    getPosts: jest.fn(),
+  };
+});
+
+describe('The getPostsSortedByTitle function', () => {
+  describe('when the getPosts function responds with a list of posts', () => {
+    beforeEach(() => {
+      getPosts.mockResolvedValue([
         {
           userId: 1,
           id: 1,
@@ -23,16 +30,24 @@ jest.mock('./getPosts', () => {
           body: 'Hello world!',
         },
       ]);
-    },
-  };
-});
+    });
+    it('should return the posts sorted by the length of the title descending', async () => {
+      const result = await getPostsSortedByTitle();
 
-describe('The getPostsSortedByTitle function', () => {
-  it('should return the posts sorted by the length of the title descending', async () => {
-    const result = await getPostsSortedByTitle();
-
-    expect(result[0].title).toBe('Very long title');
-    expect(result[1].title).toBe('Medium title');
-    expect(result[2].title).toBe('Short title');
+      expect(result[0].title).toBe('Very long title');
+      expect(result[1].title).toBe('Medium title');
+      expect(result[2].title).toBe('Short title');
+    });
+  });
+  describe('when the getPosts function responds with an error', () => {
+    beforeEach(() => {
+      getPosts.mockImplementation(() => {
+        throw new Error();
+      });
+    });
+    it('should return an empty array', async () => {
+      const result = await getPostsSortedByTitle();
+      expect(result).toEqual([]);
+    });
   });
 });
