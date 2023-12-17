29374aca639d72afe499340c13560a3a2c050d8f
pawelberowski <pawelberowski@gmail.com>
2023-12-17 18:32:18 +0100

    fix(getBMI): fixed typos, removed unnecessary else if statement and refactored to throw error when not provided with correct parameters

--- src/utilities/getBMI/getBMI.js
+++ src/utilities/getBMI/getBMI.js
@@ -1,6 +1,8 @@
 export function getBMI(weight, height) {
   if (typeof weight !== 'number' || typeof height !== 'number') {
-    return 'please provide weight as a number of kilograms and height as a number in meters';
+    throw new Error(
+      'please provide weight as a number of kilograms and height as a number in meters',
+    );
   }
   const bmi = weight / height ** 2;
   if (bmi <= 18.5) {
@@ -9,7 +11,6 @@ export function getBMI(weight, height) {
     return 'Normal';
   } else if (bmi <= 30.0) {
     return 'Overweight';
-  } else if (bmi > 30) {
-    return 'Obese';
   }
+  return 'Obese';
 }
--- src/utilities/getBMI/getBMI.test.js
+++ src/utilities/getBMI/getBMI.test.js
@@ -2,25 +2,25 @@ import { getBMI } from './getBMI';
 
 describe('The getBMI function', () => {
   describe('when provided with two numbers', () => {
-    describe('when bmi under 18.5', () => {
+    describe('and when bmi is under 18.5', () => {
       it('should return Underweight', () => {
         const result = getBMI(50, 1.8);
         expect(result).toBe('Underweight');
       });
     });
-    describe('when bmi between 18.5 and 25', () => {
+    describe('and when bmi is between 18.5 and 25', () => {
       it('should return Normal', () => {
         const result = getBMI(60, 1.8);
         expect(result).toBe('Normal');
       });
     });
-    describe('when bmi between 25 and 30', () => {
+    describe('and when bmi is between 25 and 30', () => {
       it('should return Overweight', () => {
         const result = getBMI(90, 1.8);
         expect(result).toBe('Overweight');
       });
     });
-    describe('when bmi above 30', () => {
+    describe('and when bmi is above 30', () => {
       it('should return Obese', () => {
         const result = getBMI(100, 1.8);
         expect(result).toBe('Obese');
@@ -28,11 +28,14 @@ describe('The getBMI function', () => {
     });
   });
   describe('When not provided with two numbers', () => {
-    it('should return please provide weight as a number of kilograms and height as a number in meters', () => {
-      const result = getBMI(NaN, '12');
-      expect(result).toBe(
-        'please provide weight as a number of kilograms and height as a number in meters',
-      );
+    it('should throw an error with a specific message', () => {
+      try {
+        getBMI(NaN, '12');
+      } catch (error) {
+        expect(error.message).toBe(
+          'please provide weight as a number of kilograms and height as a number in meters',
+        );
+      }
     });
   });
 });
