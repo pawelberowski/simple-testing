1bead488cc73b3d827d584621111650210735b3a
pawelberowski <pawelberowski@gmail.com>
2023-12-17 19:07:28 +0100

    fix(isPalindrome): fixed a typo and created separate test cases

--- src/utilities/isPalindrome/isPalindrome.test.js
+++ src/utilities/isPalindrome/isPalindrome.test.js
@@ -1,20 +1,32 @@
 import { isPalindrome } from './isPalindrome';
 
 describe('The isPalindrome function', () => {
-  describe('when provided with palindrome', () => {
-    it('should return true', () => {
-      const result = isPalindrome('Kayak');
-      expect(result).toBe(true);
-      const result2 = isPalindrome('Was it a cat I saw');
-      expect(result2).toBe(true);
+  describe('when provided with a palindrome', () => {
+    describe('and when provided with a single word', () => {
+      it('should return true', () => {
+        const result = isPalindrome('Kayak');
+        expect(result).toBe(true);
+      });
+    });
+    describe('and when provided with a sentence', () => {
+      it('should return true', () => {
+        const result = isPalindrome('Was it a cat I saw');
+        expect(result).toBe(true);
+      });
     });
   });
   describe('when provided with a non-palindrome', () => {
-    it('should return false', () => {
-      const result = isPalindrome('Hello!');
-      expect(result).toBe(false);
-      const result2 = isPalindrome('Was it a cat or dog?');
-      expect(result2).toBe(false);
+    describe('and when provided with a single word', () => {
+      it('should return false', () => {
+        const result = isPalindrome('Hello!');
+        expect(result).toBe(false);
+      });
+    });
+    describe('and when provided with a sentence', () => {
+      it('should return false', () => {
+        const result = isPalindrome('Was it a cat or dog?');
+        expect(result).toBe(false);
+      });
     });
   });
 });
