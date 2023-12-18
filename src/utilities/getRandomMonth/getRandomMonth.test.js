3a5ffcd7df1844c96ed4f8d072a6d9dec736f0e2
pawelberowski <pawelberowski@gmail.com>
2023-12-18 14:10:43 +0100

    refactor(getRandomMonth): refactored test to apply mock implementation before each test

--- src/utilities/getRandomMonth/getRandomMonth.test.js
+++ src/utilities/getRandomMonth/getRandomMonth.test.js
@@ -6,15 +6,15 @@ jest.mock('./getRandomNumberInRange', () => ({
 }));
 
 describe('The getRandomMonth function', () => {
-  it('should call getRandomNumberInRange with the correct arguments', () => {
-    getRandomMonth();
-    expect(getRandomNumberInRange).toHaveBeenCalledWith(1, 12);
-  });
   beforeEach(() => {
     getRandomNumberInRange.mockImplementation(() => {
       return 5;
     });
-  })
+  });
+  it('should call getRandomNumberInRange with the correct arguments', () => {
+    getRandomMonth();
+    expect(getRandomNumberInRange).toHaveBeenCalledWith(1, 12);
+  });
   it('should return a valid month name', () => {
     const validMonths = [
       'January',
