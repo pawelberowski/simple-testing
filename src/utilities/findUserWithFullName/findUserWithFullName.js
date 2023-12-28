f6f4ae609617983fc54924662de56d0d6ca798dc
pawelberowski <pawelberowski@gmail.com>
2023-12-28 11:11:10 +0100

    refactor(findUserWithFullName): simplified function logic

--- src/utilities/findUserWithFullName/findUserWithFullName.js
+++ src/utilities/findUserWithFullName/findUserWithFullName.js
@@ -1,7 +1,5 @@
 export function findUserWithFullName(usersArray, fullName) {
-  const firstName = fullName.split(' ')[0];
-  const lastName = fullName.split(' ')[1];
   return usersArray.find((user) => {
-    return user.firstName === firstName && user.lastName === lastName;
+    return user.firstName + ' ' + user.lastName === fullName;
   });
 }
